d48a66800738ec35b01dba5771beb3879e343f2a
Matthew <105622101+mattwheeler-dev@users.noreply.github.com>
2025-10-05 22:36:15 -0700

    add debouncing to search to reduce queries;

--- app/ui/search.tsx
+++ app/ui/search.tsx
@@ -1,6 +1,7 @@
 'use client';
 
 import { useSearchParams, usePathname, useRouter } from 'next/navigation';
+import { useDebouncedCallback } from 'use-debounce';
 import { MagnifyingGlassIcon } from '@heroicons/react/24/outline';
 
 export default function Search({ placeholder }: { placeholder: string }) {
@@ -8,15 +9,16 @@ export default function Search({ placeholder }: { placeholder: string }) {
 	const pathname = usePathname();
 	const { replace } = useRouter();
 
-	function handleSearch(term: string) {
+	const handleSearch = useDebouncedCallback((term) => {
 		const params = new URLSearchParams(searchParams);
+
 		if (term) {
 			params.set('query', term);
 		} else {
 			params.delete('query');
 		}
 		replace(`${pathname}?${params.toString()}`);
-	}
+	}, 300);
 
 	return (
 		<div className="relative flex flex-1 flex-shrink-0">
